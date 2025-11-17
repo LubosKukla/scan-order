2847870fd91287cc629f078b6c306942f8dbd402
Jan Drugda <80648116+janko159@users.noreply.github.com>
2025-11-17 15:30:56 +0100

    Vytvorenie tabulky restaurant
    Tabulka restaurant a prepojenie vztahou s touto tabulkou a modelom

--- backend/app/Models/Address.php
+++ backend/app/Models/Address.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Address extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'street',
+        'number_of_building',
+        'PSC',
+        'city',
+    ];
+
+    public function customer()
+    {
+        return $this->hasOne(Customer::class);
+    }
 }
---
+++ backend/database/migrations/2025_11_17_123928_create_restaurants_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('address_id')->constrained('addresses')->onDelete('cascade');
+            $table->foreignId('user_id')->unique()->constrained('users')->onDelete('cascade');
+            $table->string('ico')->nullable();
+            $table->string('dic')->nullable();
+            $table->string('dic_dph')->nullable();
+            $table->string('iban')->nullable();
+            $table->boolean('is_active')->nullable();
+            $table->string('name')->nullable();
+            $table->string('name_boss')->nullable();
+            $table->string('type_of_restaurant')->nullable();
+            $table->string('description')->nullable();
+            $table->string('logo_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurants');
+    }
+};
